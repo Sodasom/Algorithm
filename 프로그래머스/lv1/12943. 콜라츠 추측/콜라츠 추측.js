b72053ba31811b1f84dc09d3a5dcd8dff46cab78
So Dasom <1219dasom@gmail.com>
2023-04-28 13:11:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    let count = 0;
+    
+    while(num !== 1) {
+        num % 2 === 0 ? num /= 2 : num = (num *3) + 1;
+        count++;
+    }
+    return count > 499 ? -1 : count;
+}
