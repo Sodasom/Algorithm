5491882b3bd6f82a8add6b90e2947a154ee59a43
So Dasom <1219dasom@gmail.com>
2023-05-12 09:01:49 +0900

    Update 부족한 금액 계산하기.js

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,5 +1,4 @@
 function solution(price, money, count) {
-    const priceStr = String(price);
     const priceArr = Array.from({length: count}, (_, idx) => (idx + 1) * price);
     const priceSum = priceArr.reduce((acc, cur) => acc + cur);
     
