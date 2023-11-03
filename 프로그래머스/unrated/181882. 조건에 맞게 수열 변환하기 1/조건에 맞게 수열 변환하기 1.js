8289718090a2f8256f3d1dfaaf72c080a7737490
So Dasom <1219dasom@gmail.com>
2023-11-03 17:09:57 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 6.54 ms, Memory: 45.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+    const result = [];
+    
+    for(let i = 0; i < arr.length; ++i) {
+        if(arr[i] >= 50 && arr[i] % 2 === 0) {
+            result.push(Math.floor(arr[i] / 2))
+        } else if(arr[i] < 50 && arr[i] % 2 === 1) {
+            result.push(arr[i] * 2)
+        } else {
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
