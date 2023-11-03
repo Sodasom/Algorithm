93950fbff905ac9babb659969e695f076a0ff0f2
So Dasom <1219dasom@gmail.com>
2023-11-03 16:19:28 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -0,0 +1,11 @@
+function solution(myString, pat) {
+    let newString = ""
+    for(let i = 0; i < myString.length; ++i) {
+        if(myString[i] === "A") {
+            newString += "B"
+        } else {
+            newString += "A"
+        }
+    }
+    return newString.includes(pat) ? 1 : 0;
+}
