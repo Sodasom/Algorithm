46ee9ffc7cbcda9adfa21a433f6ef7a071701ad8
So Dasom <1219dasom@gmail.com>
2023-11-07 19:23:41 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.88 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    const result = new Array(s.length).fill(-1)
+    for(let i = s.length - 1; i >= 0; --i) {
+        for(let j = i - 1; j >= 0; --j) {
+            if(s[i] === s[j]) {
+                result[i] = i - j;
+                break;
+            }
+        }
+    }
+    return result;
+}
